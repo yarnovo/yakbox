f34b3c555072126dbfb1b90b965de23c763574b5
yarnb <yarnb@qq.com>
2025-07-09 02:42:04 +0800

    feat: 调整 MessageInput 组件的 maxHeight 属性，从 150 增加到 300，以支持更长消息的输入

--- src/components/ChatWindow.tsx
+++ src/components/ChatWindow.tsx
@@ -89,7 +89,7 @@ export const ChatWindow = React.forwardRef<MessageListMethods, ChatWindowProps>(
 
         {/* Input Container */}
         <div className={currentTheme.inputContainer}>
-          <MessageInput placeholder={placeholder} onSend={handleSend} maxHeight={150} />
+          <MessageInput placeholder={placeholder} onSend={handleSend} maxHeight={300} />
         </div>
       </div>
     );
